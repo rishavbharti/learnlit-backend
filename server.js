c0de0f0d50ed45c559a5a2145bf80e79e7b7f91e
Rishav Bharti <rishavbharti21@gmail.com>
2022-03-16 23:12:00 +0530

    Change domain

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://learnlit.herokuapp.com'],
+    origin: ['http://localhost:3000', 'https://learnlit.vercel.app/'],
     credentials: true,
   })
 );
